9264ddfd0a670c0200f2421f4408706331e0f9d2
osohyun0224 <osohyun0224@naver.com>
2023-07-07 15:52:27 +0900

    feat: scss, 절대경로 craco 설치, footer layout 설정

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../layout/Footer/Footer';
+
+
+function Home() {
+  return (
+    <div className="home-container">
+      {/* 여기에는 홈페이지의 주요 내용이 들어갈 것입니다. */}
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
